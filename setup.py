a6318ddc261609f23a2f5563f17e24806d350f09
Trent Jurewicz <tjurewicz@gmail.com>
2009-11-19 15:43:36 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='reporting_tools',
+    version='0.01',
+    description='Reusable Django app to display Satchmo reports.',
+    author='Trent Jurewicz',
+    author_email='tjurewicz@gmail.com',
+    packages=find_packages(),
+)
