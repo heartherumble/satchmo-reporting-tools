08a0359d6fa02373998e2fd6065f9ebf4b5de2a9
Trent Jurewicz <tjurewicz@gmail.com>
2009-11-19 23:46:16 -0600

    modified products_report to use a Filter

--- reporting_tools/views.py
+++ reporting_tools/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 
 from product.models import Product
 
-from reporting_tools.filters import OrderFilterSet, ProductFilterSet
+from reporting_tools.filters import OrderFilterSet
 
 from satchmo_store.shop.models import Order, OrderItem
 
@@ -18,9 +18,6 @@ def orders_report(request, template='admin/reports/orders_report.html'):
 	
 @staff_member_required
 def products_report(request, template='admin/reports/products_report.html'):
-    qs = Product.objects.all().order_by('-total_sold'))
-    f = ProductFilterSet(request.GET, queryset=qs)
-    f.qs[:10]
-#    f.qs.annotate(average_price=Avg('orderitem__unit_price'), total_sales=Sum('orderitem__line_item_price')
-    ctx = {'f':f}
+    products = Product.objects.filter(total_sold__gt=0).order_by('-total_sold').annotate(average_price=Avg('orderitem__unit_price'), total_sales=Sum('orderitem__line_item_price'))[:10]
+    ctx = {'products':products}
     return render_to_response(template, RequestContext(request, ctx))
