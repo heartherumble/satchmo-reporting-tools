7a6609d4873e7a2d5d68d90955ec0afa28c352b7
Trent Jurewicz <tjurewicz@gmail.com>
2009-11-19 23:43:46 -0600

    modified products_report to use a Filter

--- reporting_tools/views.py
+++ reporting_tools/views.py
@@ -18,7 +18,9 @@ def orders_report(request, template='admin/reports/orders_report.html'):
 	
 @staff_member_required
 def products_report(request, template='admin/reports/products_report.html'):
-    qs = Product.objects.all().order_by('-total_sold').annotate(average_price=Avg('orderitem__unit_price'), total_sales=Sum('orderitem__line_item_price'))
+    qs = Product.objects.all().order_by('-total_sold'))
     f = ProductFilterSet(request.GET, queryset=qs)
+    f.qs[:10]
+#    f.qs.annotate(average_price=Avg('orderitem__unit_price'), total_sales=Sum('orderitem__line_item_price')
     ctx = {'f':f}
     return render_to_response(template, RequestContext(request, ctx))
